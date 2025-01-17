816ff090dd629f7f7f053b9b36b37c7dc311ef04
Ciprian Ionita <ciprian@ciprianionita.com>
2025-01-18 01:01:30 +0200

    cleaned up and changed meta description

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata = {
     title: "Show Me Your IP",
-    description: "ShowMeYourIP app",
+    description:
+        "This tool helps you quickly find out your current IP address.",
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                {children}
-            </body>
+            <body className="antialiased">{children}</body>
         </html>
     );
 }
